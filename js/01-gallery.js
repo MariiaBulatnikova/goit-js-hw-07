d07ccee1b5106f3217acb679342f823cd44d9874
dp200193bmv <92640930+dp200193bmv@users.noreply.github.com>
2022-04-30 22:52:02 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryItemsMarkUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}"  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+  })
+  .join('');
+
+const gallery = document.querySelector('div.gallery');
+
+gallery.insertAdjacentHTML('beforeend', galleryItemsMarkUp);
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+
+  const instance = createInstance(e);
+  instance.show();
+
+  document.addEventListener('keydown', isEscapeKeydown.bind(gallery, instance));
+});
+
+function createInstance(e) {
+  return basicLightbox.create(`<img  src="${e.target.getAttribute('data-source')}">`);
+}
+
+function isEscapeKeydown(instance, event) {
+  if (event.code === 'Escape') {
+    document.removeEventListener('keydown', isEscapeKeydown);
+    instance.close();
+  }
+}
